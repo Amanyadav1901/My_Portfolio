a52833fad2f3bb2bddb6057c5d8583e5c237b42d
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-04-09 23:51:51 +0530

    New Changes 09/04

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,8 @@ const Hero = () => {
           </div>
 
           <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915eff]'>
               Aman Yadav</span></h1>
             <p className={`${styles.heroSubText} mt-2
              text-white-100`}>
@@ -27,6 +28,29 @@ const Hero = () => {
           </div>
         </div>
         <ComputersCanvas/>
+
+        <div className='absolute xs:bottom-10 bottom-32 w-full
+        flex justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[54px] rounded-3xl border-4
+            border-secondary flex justify-center items-start
+            p-2'>
+              <motion.div
+                animate={{
+                  y: [0,24,0]
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop',
+                }}
+                className='w-3 h-3 rounded-full bg-secondary mb-1'
+              />
+
+            </div>
+          </a>
+
+        </div>
       </section>
   )
 }
