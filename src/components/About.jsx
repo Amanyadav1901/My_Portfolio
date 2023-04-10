81d17ff13b87173631a835b8ca093197bee027db
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-04-10 21:53:57 +0530

    updated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,14 @@ import { fadeIn, textVariant} from '../utils/motion';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <p>{title}</p>
-  )
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className=""
+      >
+      </motion.div>
+    </Tilt>  
+  ) 
 }
 
 
