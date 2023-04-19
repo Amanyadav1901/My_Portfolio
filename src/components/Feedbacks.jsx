b099a8be8a0308827e15870cd7859c50ded901e3
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-04-19 22:47:28 +0530

    updates 19.10

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+import { text } from "express";
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div className={`${styles.padding} bg-tertiary 
+      rounded-2xl min-h-[300px]`}>
+        <motion.div
+        variants={textVariant()}>
+          <p className={styles.sectionSubText}>What Others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 
+      flex flex-wrap gap-7`}>
+
+      </div>
+    </div>
   )
 }
 
